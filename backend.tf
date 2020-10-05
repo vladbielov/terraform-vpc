eb9ef6e609457a107e11cd883e336acc9da805ad
Vladyslav Bielov <68798347+vladbielov@users.noreply.github.com>
2020-10-05 13:46:02 -0500

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
     backend "s3" {
-        bucket = "jenkins-terraform-evolvecybertraining"
+        bucket = "b1bucket"
         key = "terraform_vpc/us-east-1/class/dev/infrastructure.tfstate"
         region = "us-east-1"
     }
