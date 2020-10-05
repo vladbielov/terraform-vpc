bdaa2fb8ebf217d95e76779adb523c2830329f62
Vladyslav Bielov <68798347+vladbielov@users.noreply.github.com>
2020-10-05 14:01:48 -0500

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
     backend "s3" {
         bucket = "b1bucket"
-        key = "terraform_vpc/us-east-1/class/dev/infrastructure.tfstate"
+        key = "terraform_vpc/us-east-2/class/dev/infrastructure.tfstate"
         region = "us-east-2"
     }
 }
